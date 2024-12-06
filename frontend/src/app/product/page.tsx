7c4c6007e0b93530586b57fed384bf8338c2ec51
Rahul Roshan <r4hulrosh4n@gmail.com>
2024-12-06 21:58:46 +0530

    Product

--- frontend/src/app/product/page.tsx
+++ frontend/src/app/product/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Link from "next/link";
-
-type Props = {};
+import WishlistIcon from "@/assets/icons/WishlistIcon";
+import Delivery from "@/assets/icons/Delivery";
 
 const Size = ["XS", "S", "M", "L", "XL"];
 
-const Product = (props: Props) => {
+const Product = () => {
   return (
     <div className="container flex flex-col mx-auto my-10 overflow-x-hidden">
       <div className="inline-flex items-center mb-8">
@@ -18,11 +18,11 @@ const Product = (props: Props) => {
 
       <div className="w-full flex gap-16 justify-between">
         <div className="mx-auto w-full flex gap-8">
-          <div className="w-64 flex flex-col gap-6">
-            <div className="w-full h-40 bg-secondary rounded"></div>
-            <div className="w-full h-40 bg-secondary rounded"></div>
-            <div className="w-full h-40 bg-secondary rounded"></div>
-            <div className="w-full h-40 bg-secondary rounded"></div>
+          <div className="w-64 flex flex-col justify-between overflow-hidden">
+            <div className="w-full h-36 bg-secondary rounded"></div>
+            <div className="w-full h-36 bg-secondary rounded"></div>
+            <div className="w-full h-36 bg-secondary rounded"></div>
+            <div className="w-full h-36 bg-secondary rounded"></div>
           </div>
           <div className="bg-secondary rounded w-full"></div>
         </div>
@@ -51,15 +51,41 @@ const Product = (props: Props) => {
             <p className="text-xl tracking-wide">Size:</p>
             <div className="flex gap-4 px-6">
               {Size.map((s, index) => (
-                <div className="size-8 justify-center text-center border-black border rounded hover:border-secondary2 hover:bg-secondary2 hover:text-primary flex items-center">
+                <div
+                  key={index}
+                  className="size-8 justify-center text-center border-black border rounded hover:border-secondary2 hover:bg-secondary2 hover:text-primary flex items-center"
+                >
                   <span className="text-xs font-semibold">{s}</span>
                 </div>
               ))}
             </div>
           </div>
-          <div className="flex ">
-            <div></div>
-            <div></div>
+          <div className="flex w-full items-center gap-4">
+            <div className="flex-grow rounded size-12 bg-red-500"></div>
+            <div className="flex-grow rounded size-12 bg-red-500"></div>
+            <div className="ml-1 rounded w-10 h-10 flex items-center justify-center border border-black">
+              <WishlistIcon className="p-px" />
+            </div>
+          </div>
+          <div className="w-full mt-8 rounded border border-black">
+            <div className="flex gap-4 p-6 items-center border-b border-black">
+              <Delivery className="text-black" />
+              <div>
+                <p className="font-medium">Free Delivery</p>
+                <span className="text-xs underline">
+                  Enter your postal code for Delivery Availability
+                </span>
+              </div>
+            </div>
+            <div className="flex gap-4 p-6 items-center">
+              <Delivery className="text-black" />
+              <div>
+                <p className="font-medium">Free Delivery</p>
+                <span className="text-xs">
+                  Enter your postal code for Delivery Availability
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
