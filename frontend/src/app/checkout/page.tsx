7e10b8b8e32446a1931463e8ec3edcb7c5d5c18c
Rahul Roshan <r4hulrosh4n@gmail.com>
2024-12-10 04:34:53 +0530

    CheckOut

--- frontend/src/app/checkout/page.tsx
+++ frontend/src/app/checkout/page.tsx
@@ -17,7 +17,7 @@ const CheckOut = (props: Props) => {
         <h1 className="text-3xl font-medium tracking-wide">Billing Details</h1>
         <div className="flex my-10 gap-48 justify-between">
           <div className="flex w-full flex-col">
-            <form className="flex min-w-96 pr-40 flex-col gap-8">
+            <form className="flex min-w-96 2xl:pr-40 flex-col gap-8">
               <div className="flex flex-col gap-1">
                 <label className="text-text1">
                   First Name<span className="text-secondary2/50">*</span>
@@ -67,7 +67,61 @@ const CheckOut = (props: Props) => {
               </p>
             </div>
           </div>
-          <div className="w-full flex size-96 bg-pink-100">kdkdkdk</div>
+          <div className="w-full my-10 flex flex-col gap-10">
+            <div className="flex flex-col gap-10 w-[80%]">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-8">
+                  <div className="flex items-center size-14 bg-pink-300"></div>
+                  <div>
+                    <p>LCD Monitor</p>
+                  </div>
+                </div>
+                <p>$120</p>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-8">
+                  <div className="flex items-center size-14 bg-pink-300"></div>
+                  <div>
+                    <p>LCD Monitor</p>
+                  </div>
+                </div>
+                <p>$120</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 w-[80%]">
+              <div className="flex w-full justify-between items-center border-text1 border-b pb-4">
+                <p>Subtotal:</p>
+                <p>$1023</p>
+              </div>
+              <div className="flex w-full justify-between items-center border-text1 border-b pb-4">
+                <p>Shipping:</p>
+                <p>Free</p>
+              </div>
+              <div className="flex w-full justify-between items-center">
+                <p>Total:</p>
+                <p>$1023</p>
+              </div>
+            </div>
+            <div className="flex flex-col gap-8 w-[80%]">
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                  <div className="items-center size-7 rounded-full z-0 border-2 border-black relative after:absolute active:after:size-4 after:rounded-full after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-black after:z-10"></div>
+                  <div>
+                    <p>Bank</p>
+                  </div>
+                </div>
+                <div className="w-52 h-6 bg-pink-800"></div>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                  <div className="items-center size-7 rounded-full z-0 border-2 border-black relative after:absolute active:after:size-4 after:rounded-full after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 after:bg-black after:z-10"></div>
+                  <div>
+                    <p>Cash on delivery</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
